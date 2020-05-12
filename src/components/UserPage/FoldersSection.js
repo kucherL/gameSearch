bb4b0ed419c7f7e87ec84383d5b46b7b125a014c
Елизавета Кучерова <lizakucherova25@gmail.com>
2020-05-12 23:38:56 +0300

    added button

--- src/components/UserPage/FoldersSection.js
+++ src/components/UserPage/FoldersSection.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 
 import GameItem from "../GameItem";
 
-const FoldersSection = () => {
-  return (
-    <section className="FoldersSection">
+class FoldersSection extends Component {
+  addFolder = (event) => {};
+
+  render = () => {
+    return (
+      <section className="FoldersSection">
       <select className="FoldersSection__select">
         <option value="">Please choose a folder</option>
         <option value="folder">Folder</option>
       </select>
+      <button onClick={this.addFolder}>Add new folder</button>
       <GameItem />
     </section>
-  )
+    )
+  };
 }
 
 export default FoldersSection;
