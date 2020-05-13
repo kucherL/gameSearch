462730ccac43184de6abd4d9f630d675b3c26649
Liza <lizakucherova25@gmail.com>
2020-05-13 22:53:43 +0300

    added folders handler, fetching users games

--- src/components/UserPage/FoldersSection.js
+++ src/components/UserPage/FoldersSection.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 import GameItem from "../GameItem";
 import * as actionCreators from "../../store/actions/actions";
@@ -8,6 +9,44 @@ class FoldersSection extends Component {
   state = {
     showInput: false,
     folderName: "",
+  };
+
+  componentDidMount = () => {
+    this.props.onGetUserFolders(this.props.uid);
+  };
+
+  eventHandler = (event) => {
+    this.props.onFetchGamesInFolder(this.props.uid, event.target.value);
+  };
+
+  getFoldersList = () => {
+    return this.props.folders.map((folder, index) => {
+      return (
+        <option key={index} value={folder[0]}>
+          {folder[1].title}
+        </option>
+      );
+    });
+  };
+
+  getListOfGames = () => {
+    return this.props.games.map((game, index) => {
+      return (
+        <Link
+          to="/singlePage"
+          onClick={() => this.props.onGetId(game[0])}
+          key={index}
+          className="GameItem"
+        >
+          <GameItem
+            game={game.gameData[2]}
+            description={game.gameData[3]}
+            cover={game.gameData[1]}
+            id={game.gameData[0]}
+          />
+        </Link>
+      );
+    });
   };
 
   addFolderName = async (event) => {
@@ -21,18 +60,24 @@ class FoldersSection extends Component {
   render() {
     return (
       <section className="FoldersSection">
-        <select className="FoldersSection__select">
+        <select className="FoldersSection__select" onChange={this.eventHandler}>
           <option value="">Please choose a folder</option>
-          <option value="folder">Folder</option>
+          {this.getFoldersList()}
         </select>
         <button onClick={this.showInputHandler}>Add new folder</button>
         {this.state.showInput ? (
           <>
             <input type="text" onChange={this.addFolderName} />
-            <button onClick={() => this.props.onSetNewFolder(this.props.uid, this.state.folderName)}>Submit</button>
+            <button
+              onClick={() =>
+                this.props.onSetNewFolder(this.props.uid, this.state.folderName)
+              }
+            >
+              Submit
+            </button>
           </>
         ) : null}
-        <GameItem />
+        {this.getListOfGames()}
       </section>
     );
   }
@@ -41,13 +86,20 @@ class FoldersSection extends Component {
 const mapStateToProps = (state) => {
   return {
     uid: state.user.uid,
-  }
+    folders: state.userFolders,
+    games: state.folderGames,
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onSetNewFolder: (user, title) => dispatch(actionCreators.setNewFolder(user, title))
-  }
+    onSetNewFolder: (user, title) =>
+      dispatch(actionCreators.setNewFolder(user, title)),
+    onGetUserFolders: (user) => dispatch(actionCreators.getUserFolders(user)),
+    onFetchGamesInFolder: (user, idFolder) =>
+      dispatch(actionCreators.fetchGamesInFolder(user, idFolder)),
+    onGetId: (value) => dispatch(actionCreators.getId(value)),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(FoldersSection);
