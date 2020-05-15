bf3abf11334cb555d0307e9cc87714c538913a85
Елизавета Кучерова <lizakucherova25@gmail.com>
2020-05-15 18:44:00 +0300

    added percentage chart

--- src/components/ui/Rating/Rating.js
+++ src/components/ui/Rating/Rating.js
@@ -3,8 +3,25 @@ import React from "react";
 import "./Rating.scss";
 
 const rating = (props) => (
-  <div className="Rating">
-    <p className="Rating__number">Rating: {props.children}</p>
+  <div class="Rating">
+    <svg viewBox="0 0 36 36" class="Rating__circular-chart">
+      <path
+        class="Rating__circle-bg"
+        d="M18 2.0845
+        a 15.9155 15.9155 0 0 1 0 31.831
+        a 15.9155 15.9155 0 0 1 0 -31.831"
+      />
+      <path
+        class="Rating__circle"
+        stroke-dasharray={`${props.children}, 100`}
+        d="M18 2.0845
+        a 15.9155 15.9155 0 0 1 0 31.831
+        a 15.9155 15.9155 0 0 1 0 -31.831"
+      />
+      <text x="18" y="20.35" class="Rating__percentage">
+        {`${props.children}%`}
+      </text>
+    </svg>
   </div>
 );
 
